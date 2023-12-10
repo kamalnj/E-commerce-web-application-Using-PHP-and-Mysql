e0587cb066fe4545c1dccb52b1293fac0e077f42
kamalnj <99343671+kamalnj@users.noreply.github.com>
2023-12-10 10:50:04 +0100

    Updating navbar

--- user/nav.php
+++ user/nav.php
@@ -12,45 +12,89 @@
 <script src="https://cdn.tailwindcss.com/3.3.0"></script>
 </head>
 <body>
-      <!-- navbar -->
-    <nav class=" justify-between bg-gray-50 text-white h-16 ">
-      <div class="px-5 py-4  xl:px-12  flex w-full items-center">
-        <a class="text-3xl font-bold font-heading" href="#">
-          <!-- <img class="h-9" src="logo.png" alt="logo"> -->
-        <img class="  w-32" src="https://planetsport.ma/img/planet-sport-logo-1657626409.jpg" alt="">
+<header class=' bg-black px-6 h-15  '>
+      <div class='flex h-15 items-center justify-between  '>
+        <a href="javascript:void(0)"><img
+          src="https://logos-world.net/wp-content/uploads/2022/04/Foot-Locker-Logo.png" alt="logo" class='w-36  ' />
         </a>
-        <!-- Nav Links -->
-        <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-          <li><a class="text-black  hover:text-stone-600" href="#">Hommes</a></li>
-          <li><a class="text-black  hover:text-stone-600" href="#">Femmes</a></li>
-          <li><a class="text-black hover:text-stone-600" href="#">Enfants</a></li>
-          <li><a class="text-black  hover:text-stone-600" href="#">Accessoires</a></li>
-        </ul>
-        <!-- Header Icons -->
-        <div class="hidden xl:flex items-center space-x-5">
-        <div class="flex items-center">
-    <input type="text" placeholder="Search..." class="border border-gray-300 text-black px-2 py-1 rounded-l-md focus:outline-none">
-    <a class="text-black hover:text-gray-200 bg-gray-300 px-2 py-1 rounded-r-md" href="your_search_url">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-            <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z" clip-rule="evenodd" />
-        </svg>
-    </a>
-</div> 
-        <a class="flex items-center text-black  hover:text-gray-200" href="#">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-              <path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z" clip-rule="evenodd" />
+        <div class='flex items-center max-lg:ml-auto lg:order-1'>
+        <div class=" px-3 relative mx-auto text-gray-600">
+        <input class="border-2 border-gray-300 bg-white h-9 px-5  rounded-xl text-sm focus:outline-none"
+          type="search" name="search" placeholder="Search">
+        <button type="submit" class="absolute right-0 top-0 py-3 px-5">
+          <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
+            viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
+            width="512px" height="512px">
+            <path
+              d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
           </svg>
-          </a>
-          <a class="flex items-center text-black" href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-          </a>
-
-        </div>
+        </button>
       </div>
-      
-    </nav>
+    <a href="" class="flex items-center mr-4 dark:text-gray-400 ">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+</svg>
+
+           </a>
+           <a href="" class="flex items-center mr-4 dark:text-gray-400 ">
+           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-6 h-6">
+  <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
+</svg>
+
+
+           </a>
+        </div>
+        <ul class='flex max-lg:block max-lg:w-full lg:space-x-10 '>
+
+          <li class='group max-lg:border-b max-lg:py-2 relative'>
+            <a href='javascript:void(0)'
+              class='hover:text-yellow-300 font-bold text-white text-[15px] lg:hover:fill-yellow-300 block'>Homme
+            </a>
+            <ul
+              class='absolute hidden group-hover:block shadow-lg max-lg:border max-lg:border-gray-500 bg-black px-6 pb-4 pt-6 space-y-3 lg:top-5 max-lg:top-8 lg:-left-6 min-w-[250px] z-50'>
+              <li class='border-b py-2'><a href='javascript:void(0)'
+                class='hover:text-yellow-300 font-bold text-white text-[15px] block'>Chaussures</a></li>
+              <li class='border-b py-2'><a href='javascript:void(0)'
+                class='hover:text-yellow-300 font-bold text-white text-[15px] block'>Vêtements</a></li>
+              <li class='border-b py-2'><a href='javascript:void(0)'
+                class='hover:text-yellow-300 font-bold text-white text-[15px] block'>Accessoires et équipement</a></li>
+
+            </ul>
+          </li>
+          <li class='group max-lg:border-b max-lg:py-2 relative'>
+            <a href='#'
+              class='hover:text-yellow-300 font-bold text-white text-[15px] lg:hover:fill-yellow-300 block'>Femme
+            </a>
+            <ul
+              class='absolute hidden group-hover:block shadow-lg max-lg:border max-lg:border-gray-500 bg-black px-6 pb-4 pt-6 space-y-3 lg:top-5 max-lg:top-8 lg:-left-6 min-w-[250px] z-50'>
+              <li class='border-b py-2'><a href='javascript:void(0)'
+                class='hover:text-yellow-300 font-bold text-white text-[15px] block'>Chaussures</a></li>
+              <li class='border-b py-2'><a href='javascript:void(0)'
+                class='hover:text-yellow-300 font-bold text-white text-[15px] block'>Vêtements</a></li>
+              <li class='border-b py-2'><a href='javascript:void(0)'
+                class='hover:text-yellow-300 font-bold text-white text-[15px] block'>Accessoires et équipement</a></li>
+            </ul>
+          </li>
+          <li class='group max-lg:border-b max-lg:py-2 relative'>
+            <a href='#'
+              class='hover:text-yellow-300 font-bold text-white text-[15px] lg:hover:fill-yellow-300 block'>Enfant
+            </a>
+            <ul
+              class='absolute hidden group-hover:block shadow-lg max-lg:border max-lg:border-gray-500 bg-black px-6 pb-4 pt-6 space-y-3 lg:top-5 max-lg:top-8 lg:-left-6 min-w-[250px] z-50'>
+              <li class='border-b py-2'><a href='javascript:void(0)'
+                class='hover:text-yellow-300 font-bold text-white text-[15px] block'>Chaussures</a></li>
+              <li class='border-b py-2'><a href='javascript:void(0)'
+                class='hover:text-yellow-300 font-bold text-white text-[15px] block'>Vêtements</a></li>
+              <li class='border-b py-2'><a href='javascript:void(0)'
+                class='hover:text-yellow-300 font-bold text-white text-[15px] block'>Accessoires et équipement</a></li>
+            </ul>
+          </li>
+          <li class='max-lg:border-b max-lg:py-2'><a href='javascript:void(0)'
+            class='hover:text-yellow-300 font-bold text-white text-[15px] block'>Nouveautés</a></li>
+
+      </div>
+    </header>
 
 
 
